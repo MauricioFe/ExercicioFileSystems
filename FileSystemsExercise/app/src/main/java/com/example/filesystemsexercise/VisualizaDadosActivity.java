1ff041c620616a10a7afdd39dc6ef535414a9eae
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-08 10:14:12 -0300

    Criando activity para mostrar dados salvos em arquivo

---
+++ FileSystemsExercise/app/src/main/java/com/example/filesystemsexercise/VisualizaDadosActivity.java
@@ -0,0 +1,14 @@
+package com.example.filesystemsexercise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VisualizaDadosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualiza_dados);
+    }
+}
