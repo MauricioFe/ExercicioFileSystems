a5cf18d47d36abe97da1456456972596418d9204
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-08 10:12:50 -0300

    Criação do projeto e do método para salvar o nome e sobrenome do usuário em um arquivo externo

---
+++ FileSystemsExercise/app/src/main/java/com/example/filesystemsexercise/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.filesystemsexercise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText edtNome = findViewById(R.id.edtNome);
+        final EditText edtSobrenome = findViewById(R.id.edtSobrenome);
+        Button btnEnviar = findViewById(R.id.btnEnviar);
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sobrenome = edtSobrenome.getText().toString();
+                String nome = edtNome.getText().toString();
+                try{
+                    FileOutputStream fileOutputStream = openFileOutput("usuarios",MODE_PRIVATE);
+                    fileOutputStream.write(nome.getBytes());
+                    fileOutputStream.write(sobrenome.getBytes());
+                    fileOutputStream.close();
+                    fileOutputStream.flush();
+
+                    Toast.makeText(MainActivity.this, "Usuário salvo com sucesso", Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
